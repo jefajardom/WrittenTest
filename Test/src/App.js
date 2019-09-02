2437069488bdaf109452f5ad8b7e76aafd452003
jefajardom <54727997+jefajardom@users.noreply.github.com>
2019-09-01 19:24:50 -0500

    Add files via upload

---
+++ Test/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import List from './List'
+
+
+function App() {
+  return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 mx-auto">
+            <h1 className="text-center">Written Test</h1>
+            <List />
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default App;
